7c994a7027167d9a7c0cd580630386538f7cbb9c
MatheusPontes27 <matheusemmanuel71@gmail.com>
2025-04-30 23:06:48 -0300

    update mobile 1

--- script.js
+++ script.js
@@ -80,5 +80,13 @@ const slides = document.querySelectorAll('.intro-images .slide');
     });
   });
 
+  const toggle = document.querySelector('.menu-toggle');
+  const menu = document.querySelector('.menu');
+
+  toggle.addEventListener('click', () => {
+    toggle.classList.toggle('active');
+    menu.classList.toggle('show');
+  });
+
     
      
